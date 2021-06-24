20796d12b13b5e34fc35783bf55534c256088fae
Antonia Berg <aberg@tecracer.de>
2021-06-24 15:50:09 +0200

    auto vars & backend adapted for demo

--- demo-cluster/config.tf
+++ demo-cluster/config.tf
@@ -1,16 +1,11 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.0.0"
 
   required_providers {
     aws = "= 3.44.0"
   }
 
-
-  ## change to your own backend 
-  backend "s3" {
-    bucket = "tfstatebucket-aberg"
-    key    = "tf-project/ecs-anywhere-cluster/terraform.tfstate"
-    region = "eu-central-1"
+  backend "local" {
   }
 }
 
